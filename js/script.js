acda1ebba36e397febc793d11fae99e507eb9061
teemaYouyoh <mrjokerok21@gmail.com>
2019-09-30 10:42:05 +0300

    Firts commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+	$('#elastislide').elastislide();
+});
+
+$(window).load(function(){
+	var carouselCaptionWidth = $('#carousel-sidebar .active img').width();
+	$('#carousel-sidebar img').each(function(){
+		$(this).attr('width', carouselCaptionWidth);
+	});
+
+	$('#carousel-sidebar .sidebar-carousel-caption').css('max-width', carouselCaptionWidth + 'px'); 
+	$("#carousel-sidebar .carousel-indicators ").css('max-width', carouselCaptionWidth + 'px'); 
+	$(".sidebar .banner").css('max-width', carouselCaptionWidth + 'px'); 
+
+});
